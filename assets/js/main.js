d790d287ec8eec83d07bdde0c6a35604c0734a33
Dirk Jasper <djasper@mac7.local>
2020-10-27 16:24:52 +0100

    PortfolioSeite_v01

---
+++ assets/js/main.js
@@ -0,0 +1,110 @@
+// function myFunction() {
+//   document.getElementById("test").style.color = "red";
+// }
+// This is a single line JavaScript comment
+// document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+
+// document.write("I have multi-line comments!");
+/*document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");*/
+
+// a = 12;
+// b = a * 3;
+// console.log(b);
+// document.write(b);
+
+// console.log("Anton");
+// let name = "Anton"
+// console.log(name)
+// let age = "28"
+// console.log(age)
+// let job = "Trainer"
+// console.log(job)
+// var married = true
+// console.log(married)
+// console.log(name + " ist " + age + " Jahre alt, " + job + " und verheiratet: " + married)
+
+// let myText = "Hello again"
+// document.write(myText)
+// let Name = "John"
+// let Nachname = "Schmidt"
+// document.write("<br>" + "Name: " + Name + "<br>" + "Nachname: " + Nachname)
+
+// let carName = "BMW"
+// console.log(carName)
+// let x = 150
+// console.log(x)
+// let y = 50
+// console.log(y)
+// let z = x + y
+// console.log(z)
+let firstName = "John"
+let lastName = "Doe"
+let age = 35
+console.log(firstName + lastName + age)
+
+var x = "Hello"
+var y = "World <br>"
+var z = x + y
+document.write(z)
+var n = x + " " + y
+document.write(n)
+x = x + "World"
+x += "World"
+document.write(x)
+
+var x = "ich komme zuerst"
+var y = "Ich komme auf Platz 2"
+console.log(x + y)
+
+var score = 5 + 5 * 10
+console.log("Ergebnis: " + score)
+
+var score = (5 + 5) * 10
+console.log("Ergebnis: " + score)
+
+var score = 5
+score = score + 10
+console.log("Ergebnis: " + score)
+
+var score = 2
+score += 10
+console.log("Ergebnis: " + score)
+
+var zahl = 16
+zahl+=8
+console.log("increment: " + zahl)
+
+zahl ++
+console.log("increment: " + zahl)
+
+zahl--
+console.log("increment: " + zahl)
+
+// Addition
+var addition_operator = 1 + 1;
+console.log("addition: " + addition_operator);
+
+// Subtraktion
+var subtraktion_operator = 2 - 1;
+console.log("subtraktion: " + subtraktion_operator);
+
+// Multiplikation
+var multiplication_operator = 2 * 2;
+console.log("multiplication: " + multiplication_operator);
+
+// Division
+var division_operator = 4 / 2;
+console.log("division: " + division_operator);
+
+// Modul: zeigt an, was der Rest sein würde.
+var modulus_operator = 14 % 5;
+console.log("modulus: " + modulus_operator);
+
